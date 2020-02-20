80f615374c5b50e2901d8ee10549770fdeb175d3
Nicole Gibson <nicolegibson@Nicole-Gibsons-MacBook-Pro.local>
2020-02-20 12:52:45 -0600

    homepage

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './menu-item.styles.scss'
+
+//functional component, does not need to hold state
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div className = 'background-image'
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
+            <div className='content'>
+                <h1 className ='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+    </div>
+);
+export default MenuItem;
