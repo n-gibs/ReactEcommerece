8e58daa0b10aaaf36dcffcf443a51f9e6d15a371
Nicole Gibson <nicolegibson@Nicole-Gibsons-MacBook-Pro.local>
2020-02-20 14:44:38 -0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-//import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+        <Route exact path='/' component={HomePage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+//higher order component^^
+
 import './menu-item.styles.scss'
 
 //functional component, does not need to hold state
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+    <div 
+    className={`${size} menu-item`}
+    onClick={()=> history.push(`${match.url}${linkUrl}`)}
+    >
         <div className = 'background-image'
         style={{
             backgroundImage: `url(${imageUrl})`
@@ -14,4 +20,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
             </div>
     </div>
 );
-export default MenuItem;
+//location, match and history props
+export default withRouter(MenuItem);
