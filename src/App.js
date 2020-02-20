7aebc56443b6258cb8eb3e996e50eb6739b40a94
nikkig124 <nikkig124@gmail.com>
2020-02-20 15:34:20 -0600

    collection prevew and item

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shoppage.components';
 
 
 function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+
     </div>
   );
 }
 
 export default App;
+
+
