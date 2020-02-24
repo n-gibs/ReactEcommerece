f10f3130e57c4efa634410e87895f5d748823769
nikkig124 <nikkig124@gmail.com>
2020-02-24 15:42:48 -0600

    added cart icon to the header, no functionality yet

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import './header.styles.scss';
 import { ReactComponent as Logo }  from '../../assets/crown.svg';
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='./signin'> SIGN IN</Link>
             }
+            <CartIcon/>
         </div>
     </div>
 );
