5821b4b65840898b2234cc729be86600bb266ee3
nikkig124 <nikkig124@gmail.com>
2020-02-28 13:53:50 -0600

    added styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
 
@@ -9,33 +9,35 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-import './header.styles.scss';
+//import './header.styles.scss';
 import { ReactComponent as Logo }  from '../../assets/crown.svg';
+
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv} from './header.styles'
 
 
 const Header = ({ currentUser, hidden }) => (
-    <div className ='header'>
-        <Link to ="/">
+    <HeaderContainer>
+        <LogoContainer to ="/">
             <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ? 
-                <div className='option' onClick = {() => auth.signOut()}>SIGN OUT</div>
+                <OptionDiv onClick = {() => auth.signOut()}>SIGN OUT</OptionDiv>
                 :
-                <Link className='option' to='./signin'> SIGN IN</Link>
+                <OptionLink to='./signin'> SIGN IN</OptionLink>
             }
             <CartIcon/>
-        </div>
+        </OptionsContainer>
         {hidden ? null :
         <CartDropdown/>}
-    </div>
+    </HeaderContainer>
 );
 
 //automatically passes state
